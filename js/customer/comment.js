95e9d3855965b9e263270eb024c821c6dd89834f
scauwjh <scauwjh@gmail.com>
2014-02-01 12:54:56 +0800

    A probrem of disqus
    Signed-off-by: scauwjh <scauwjh@gmail.com>

--- js/customer/comment.js
+++ js/customer/comment.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-	var disqus_shortname = 'scauwjh';
 	var dsq = document.createElement('script'); 
 	dsq.type = 'text/javascript'; 
 	dsq.async = true;
-	dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+	dsq.src = 'http://scauwjh.disqus.com/embed.js';
 	//alert(123);
-	var obj = document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0];
-    obj.appendChild(dsq);
+	(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(dsq);
     //add back to top icon
     backToTopIcon(1000);
 });

484ef207faa43f4e97966295f6d306a3763f8328
scauwjh <scauwjh@gmail.com>
2014-02-01 13:03:07 +0800

    disqus test
    Signed-off-by: scauwjh <scauwjh@gmail.com>

--- js/customer/comment.js
+++ js/customer/comment.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-	var dsq = document.createElement('script'); 
-	dsq.type = 'text/javascript'; 
-	dsq.async = true;
-	dsq.src = 'http://scauwjh.disqus.com/embed.js';
+	// var dsq = document.createElement('script'); 
+	// dsq.type = 'text/javascript'; 
+	// dsq.async = true;
+	// dsq.src = 'http://scauwjh.disqus.com/embed.js';
+	// (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(dsq);
+
+	var reqUrl = 'http://scauwjh.disqus.com/embed.js';
+	$.post(reqUrl,{},function(data){
+		comment = data;
+	});
+	
 	//alert(123);
-	(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(dsq);
+	
     //add back to top icon
     backToTopIcon(1000);
 });

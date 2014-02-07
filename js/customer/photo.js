58dc41c09a1f4bb35f0a70ca823cbc95f0117bf6
scauwjh <scauwjh@gmail.com>
2014-02-07 23:11:23 +0800

    imporve the photo moduel add a lot of picture
    Signed-off-by: scauwjh <scauwjh@gmail.com>

--- js/customer/photo.js
+++ js/customer/photo.js
@@ -1,18 +1,30 @@
 $(document).ready(function(){
-	var picNum = 5;
-	var albumNum = 1;
+	var picNum = 10;
+	var albumNum = 4;
 	var fileNum = new Array();
+	var group = new Array();
 	fileNum[0] = 5;
-	fileNum[1] = 1;
-	fileNum[2] = 1;
-	fileNum[3] = 1;
-	fileNum[4] = 1;
-	fileNum[5] = 1;
+	fileNum[1] = 18;
+	fileNum[2] = 67;
+	fileNum[3] = 11;
 	var str = "";
 	var path = "";
+	var count = 0;
+	for (var i = 0; i < albumNum; i++) {
+		for (var j = 0; j < fileNum[i]; j++){
+			group[count++] = i*1000 + j;
+		}
+	}
 	for(var i = 0; i < picNum; i++){
-		var folder = Math.floor(Math.random() * albumNum) + 1;
-		var file = Math.floor(Math.random() * fileNum[folder-1]) + 1;
+		var rand = Math.floor(Math.random() * count);
+		// var file = Math.floor(Math.random() * fileNum[folder-1]) + 1;
+		var file = group[rand] % 1000 + 1;//每个文件夹里面的图片总量不超过1000
+		var folder = (group[rand] - file + 1) / 1000 +1;
+		for (var j = rand; j < count; j++) {
+			group[j] = group[j+1];
+		}
+		count--;
+		//alert(folder+ "," + file);
 		path = 'img/photo/album' + folder + '/' + file + '.jpg';
 		if (i == 0) {
 			str = '<div class="item active">';
@@ -20,9 +32,11 @@ $(document).ready(function(){
 			str += '<div class="item">';
 		}
 		// str += '<div style="background-image:url(' + path + ');background-repeat:no-repeat;';
-		// str += 'background-size:auto 368px;margin-top:15px;width:auto;height:368px;"></div>';
+		// str += 'background-size:auto 368px;margin-top:15px;width:368px;height:368px;"></div>';
+		str += '<div style="width:380px;height:380px;text-align:center">';
 		str += '<img src="' + path + '"';
 		str += 'onerror="javascript:this.src=\'img/photo/nopic.jpg\'" class="photo-carousel-img">';
+		str += '</div>';
 		str += '</div>';
 	}
 	$("#photo_carousel .carousel-inner").append(str);
